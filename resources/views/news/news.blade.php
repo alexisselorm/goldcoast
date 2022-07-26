ac7a53b196a10500af69349093b154ce6409bdd1
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-07-26 23:34:01 +0100

    Working on news page

--- resources/views/news/news.blade.php
+++ resources/views/news/news.blade.php
@@ -1,1 +1,34 @@
-Pour the fucking news here
+<x-guest-layout>
+    <style>
+        body {
+            font-family: 'Nunito', sans-serif;
+        }
+    </style>
+    </head>
+
+    <body class="antialiased bg-[#f5f5f5]">
+
+
+        @if (Route::has('login'))
+            <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                @auth
+                @else
+                    <a href="{{ route('login') }}" class="text-m text-white dark:text-gray-500 ">Log in</a>
+
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}" class="ml-4 text-m text-white dark:text-gray-500">Register</a>
+                    @endif
+                @endauth
+            </div>
+        @endif
+
+        <div class="">
+            @include('layouts.users.navigation')
+        </div>
+
+        {{-- News Card --}}
+
+        {{-- End of News Card --}}
+        </div>
+    </body>
+</x-guest-layout>

6a496e884ace48101d572a00eca45d0d815e739a
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-08-03 18:21:45 +0100

    News page 10% done. Looks lokay

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -10,16 +10,16 @@ class NewsController extends Controller
     public function all()
     {
         return view('news.news',
-        ['news'=>News::latest()->with('author')->simplePaginate(12)->withQueryString(),
+            ['news' => News::latest()->with('author')->simplePaginate(12),
 
-        ]);
+            ]);
     }
 
     public function single_news(News $single_news)
     {
         return view('news.single_news',
-        ['single_news'=>$single_news,
+            ['single_news' => $single_news,
 
-        ]);
+            ]);
     }
 }
--- resources/views/news/single_news.blade.php
+++ resources/views/news/single_news.blade.php
@@ -1,32 +1,41 @@
 <x-guest-layout>
 
+    <div class="mx-auto bg-gray-800 max-w-7xl sm:px-6 lg:px-8">
+        <div class="text-white text-start">
+            <div class="grid gap-2 p-4 md:grid-cols-4 md:grid-rows-3">
+                <div class="p-4 md:col-span-3">
+                    {!! $single_news->body !!}
+                </div>
+                    <div class="p-4 md:col-span-3">
+        {{-- Reply section --}}
 
-    {!! $single_news->body !!}
+        {{-- View Comment --}}
+        <h4>Display Comments</h4>
+        @include('partials._comment_replies', [
+            'comments' => $single_news->comments,
+            'news_id' => $single_news->id,
+        ])
 
+        {{-- Add Comments --}}
+        <hr />
 
-    {{-- Reply section --}}
-
-    {{-- View Comment --}}
-    <h4>Display Comments</h4>
-    @include('partials._comment_replies', [
-        'comments' => $single_news->comments,
-        'news_id' => $single_news->id,
-    ])
-
-    {{-- Add Comments --}}
-    <hr />
-
-    <h4>Add comment</h4>
-    <form method="post" action="{{ route('comment.add') }}">
-        @csrf
-        <div class="form-group">
-            <input type="text" name="comment_body" class="form-control" />
-            <input type="hidden" name="news_id" value={{ $single_news->id }} />
-            {{-- {{dd($single_news->id)}} --}}
+        <h4>Add comment</h4>
+        <form method="post" action="{{ route('comment.add') }}">
+            @csrf
+            <div class="form-group">
+                <input type="text" name="comment_body" class="form-control" />
+                <input type="hidden" name="news_id" value={{ $single_news->id }} />
+                {{-- {{dd($single_news->id)}} --}}
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-warning" value="Add Comment" />
+            </div>
+        </form>
+    </div>
+            </div>
         </div>
-        <div class="form-group">
-            <input type="submit" class="btn btn-warning" value="Add Comment" />
-        </div>
-    </form>
+    </div>
+
+
     {{-- End of replies --}}
 </x-guest-layout>

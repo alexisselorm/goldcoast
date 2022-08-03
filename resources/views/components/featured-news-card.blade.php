4de5e4203fd16cf7d7e50b6e29a720109559d398
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-08-03 14:52:15 +0100

    Text Overlay added. Responsive on mobile and desktop

--- resources/views/components/featured-news-card.blade.php
+++ resources/views/components/featured-news-card.blade.php
@@ -1,5 +1,5 @@
 {{-- Jumbo div --}}
-<div class=" md:col-span-3 md:row-span-3">
+<div class="relative md:col-span-3 md:row-span-3">
     {{-- Major news card --}}
     <a href="/news/2022/august/new-fanzone-record-set-for-bolton-game/" class="news-grid-article hasImage">
         <div class="image-container" data-imagetype="inline"
@@ -8,18 +8,14 @@
             data-media-lg="/siteassets/image/202223/2223-season/bolton-wanderers-h/fanzone-bolton-h.png/Large"><img
                 class="reponsive-placeholder"
                 src="https://www.itfc.co.uk/siteassets/image/202223/2223-season/bolton-wanderers-h/fanzone-bolton-h.png/Large">
+
         </div>
-        <div class="overlay"></div>
-        <div class="inner">
-            <div class="detail-block">
-                <div class="block-inner">
-                    <div class="news-detail-wrap">
-                        <span class="small">Club News</span>
-                        <h2>FANZONE RECORD BROKEN AGAIN</h2>
-                        <span class="date detail">17 Hours ago</span>
-                    </div>
-                </div>
-            </div>
+
+
+        <div class="absolute text-white bottom-5 left-5">
+            <span class="small">Club News</span>
+            <h2 class="text-2xl">FANZONE RECORD BROKEN AGAIN</h2>
+            <span class="date detail">17 Hours ago</span>
         </div>
     </a>
     {{-- End of news --}}

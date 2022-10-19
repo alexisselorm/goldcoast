c6a64f7de94a85e6f0c65e5e6e4532d7a21f3019
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-10-19 22:43:30 +0100

    Fixed styling for upcoming matches

--- resources/views/fixtures/index.blade.php
+++ resources/views/fixtures/index.blade.php
@@ -1,41 +1,62 @@
 <x-guest-layout>
 
 
-    <x-section-container>(
+    <x-section-container>
         {{-- {{ dd($fixturesByMonth) }} --}}
         <x-next-fixture :latest_fixture="$latest_fixture" />
 
-        @foreach($fixturesByMonth as $monthlyfixtures)
-{{-- {{ dd($monthlyfixtures) }} --}}
+        @foreach ($fixturesByMonth as $monthlyfixtures)
+            {{-- {{ dd($monthlyfixtures) }} --}}
 
-        <div class="px-4  text-gray-500 md:col-span-3">
-            {{ Carbon\Carbon::parse($monthlyfixtures[0]->gametime)->format('F') }} 2022
-            <hr class="mt-1">
-        </div>
-        <div class="px-4 md:col-span-3">
-            <div class="mb-3">
-                <hr class="mt-1">
-                @foreach($monthlyfixtures as $fixture)
-                {{$fixture->competition->name}} - Date
+            <div class="px-4 font-semibold text-lg text-gray-500 md:col-span-3">
+                {{ strtoupper(Carbon\Carbon::parse($monthlyfixtures[0]->gametime)->format('F Y')) }}
+                <hr class="mt-3">
             </div>
-            <div class="p-4 my-1 flex flex-wrap justify-between">
-                <div>
-                    Home team logo
-                </div>
-                <div class="flex flex-wrap">
-                    <div>Ajax</div>
-                    <div class="px-3 mx-2 bg-gray-100 rounded txt-gray-800">19:45</div>
-                    <div>Liverpool</div>
-                </div>
+            @foreach ($monthlyfixtures as $fixture)
+                @php
+                    if ($fixture->isHome) {
+                        $home = 'GoldCoast FC';
+                        $away = $fixture->opponent->name;
+                        $stadium = 'Bravide Arena';
+                    } else {
+                        $home = $fixture->opponent->name;
+                        $away = 'GoldCoast FC';
+                        $stadium = $fixture->opponent->stadium;
+                    }
 
-                <div>
-                    Away team logo
+                @endphp
+                <div class="p-6">
+                    <div class="">
+                        {{Carbon\Carbon::parse($fixture->gametime)->format('d F')}} - {{ $stadium }}
+                    </div>
+                    <div>
+                        {{ $home }}
+                    </div>
                 </div>
-            </div>
+                <div class="p-4 items-center text-center">
+                    <div class="invisible">
+                        Here
+                    </div>
+                    <div class="flex items-center justify-center align-center">
+                        <div>Home Logo</div>
+                        <div class="px-3 mx-2 bg-gray-100 rounded text-gray-800">
+                            {{ substr($fixture->gametime, 11, -3) }}</div>
+                        <div>Away Logo</div>
+                    </div>
+
+                </div>
+                <div class="p-4 items-end justify-end  text-end">
+                    <div class="invisible">
+                        Here
+                    </div>
+                    <div>
+                        {{ $away }}
+                    </div>
+                </div>
             @endforeach
-        </div>
         @endforeach
 
     </x-section-container>
+
 
 </x-guest-layout>

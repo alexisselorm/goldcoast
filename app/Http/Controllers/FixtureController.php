2c9af7a6edf5ecd8092859f91b43dd7d86abd401
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-11-24 22:27:31 +0000

    Debugging date groups checks

--- app/Http/Controllers/FixtureController.php
+++ app/Http/Controllers/FixtureController.php
@@ -4,28 +4,25 @@ namespace App\Http\Controllers;
 
 use App\Models\Fixture;
 use Carbon\Carbon;
-use Illuminate\Http\Request;
 
 class FixtureController extends Controller
 {
     public function index(){
 
-        $currentdate = Carbon::now();
+        $currentdate = date(now());
         $game = Fixture::with('competition')->orderBy('gametime','asc')->first();
-
+        // dd(Carbon::parse($game->gametime)->format('M'));
+        // dd(Carbon::parse(now())->format('M'));
+        // dd(Carbon::parse($game->gametime)->format('M') > Carbon::parse(now())->format('M'));
 
         return view('fixtures.index',[
         'latest_fixture' => Fixture::with('competition')->orderBy('gametime','desc')->first(),
-        'fixturesByMonth'=> Fixture::with('competition')->orderBy('gametime','desc')->get()->groupBy(function($fixt) use($currentdate){
-            // dd(Carbon::parse($fixt->gametime));
+        'fixturesByMonth'=> Fixture::with('competition')->orderBy('gametime','desc')->get()->groupBy(function($fixt) {
             // Only show fixtures from the current month to the months ahead
-            if(Carbon::parse($fixt->gametime) >= $currentdate){
-                return Carbon::parse($fixt->gametime)->format('M');
-            }
-            else{
-                // continue;
+            $months =Carbon::parse($fixt->gametime)->format('M');
+            // dd($months);
+            return $months;
 
-            }
         })
     ]);
     }

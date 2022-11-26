9d3c75144cb0532fe78afadcf940fe4b8bfdcd7a
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-11-26 01:18:51 +0000

    Need to fix failing image uploads. ckeditor and intervention

--- database/migrations/2022_09_12_112242_create_competitions_table.php
+++ database/migrations/2022_09_12_112242_create_competitions_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('slug');
-            $table->string('competition_logo');
+            $table->string('competition_logo')->nullable();
             $table->timestamps();
         });
     }

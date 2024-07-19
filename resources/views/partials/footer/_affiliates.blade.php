604da230bfebcc388901cdab3de3a63cbf6ca9c1
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2024-07-19 10:33:34 +0200

    Mock names

--- resources/views/partials/footer/_affiliates.blade.php
+++ resources/views/partials/footer/_affiliates.blade.php
@@ -2,10 +2,10 @@
     <h2 class="mb-6 text-sm font-semibold text-gray-500 uppercase dark:text-gray-400">AFFILIATED CLUBS</h2>
     <ul class="text-gray-500 dark:text-gray-400">
         <li class="mb-4">
-            <a href="#" class="hover:underline">Kortedala IKF</a>
+            <a href="#" class="hover:underline">Vindicators 3</a>
         </li>
         <li class="mb-4">
-            <a href="#" class="hover:underline">Liverpool</a>
+            <a href="#" class="hover:underline">Superficial Team</a>
         </li>
 
     </ul>
--- resources/views/partials/footer/_sponsors.blade.php
+++ resources/views/partials/footer/_sponsors.blade.php
@@ -1,14 +1,14 @@
 <div>
-            <h2 class="mb-6 text-sm font-semibold text-gray-500 uppercase dark:text-gray-400">SPONSORS</h2>
-            <ul class="text-gray-500 dark:text-gray-400">
-                <li class="mb-4">
-                    <a href="#" class="hover:underline">SKF</a>
-                </li>
-                <li class="mb-4">
-                    <a href="#" class="hover:underline">Göteborg IFK</a>
-                </li>
-                <li class="mb-4">
-                    <a href="#" class="hover:underline">Facebook</a>
-                </li>
-            </ul>
-        </div>
+    <h2 class="mb-6 text-sm font-semibold text-gray-500 uppercase dark:text-gray-400">SPONSORS</h2>
+    <ul class="text-gray-500 dark:text-gray-400">
+        <li class="mb-4">
+            <a href="#" class="hover:underline">AEIOU A/B</a>
+        </li>
+        <li class="mb-4">
+            <a href="#" class="hover:underline">Wubba Lubba Dub Dub</a>
+        </li>
+        <li class="mb-4">
+            <a href="#" class="hover:underline">Tycoon Inc</a>
+        </li>
+    </ul>
+</div>
